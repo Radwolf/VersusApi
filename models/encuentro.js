33349ce750b56bdcde9d020569a664268b3391ea
Rul Gomo <raul.gomo@gmail.com>
2013-12-18 06:54:03 +0100

    Coordenadas a string

--- models/encuentro.js
+++ models/encuentro.js
@@ -5,8 +5,8 @@ var encuentroSchema = new Schema({
   retador:	{ type: String, required: true },
   retado:	{ type: String},
   actividad:{ type: String, required:true },
-  longitud :{ type: Number, required:true },
-  latitud  :{ type: Number, required:true },
+  longitud :{ type: String, required:true },
+  latitud  :{ type: String, required:true },
   resultado:{ type: String}
 });
 
